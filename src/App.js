3c68e433dca671a7c860581c514b5106a9fdbb08
Percy Mamedy <percymamedy@gmail.com>
2020-03-30 14:12:15 +0400

    Changed app text

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-         I was changed
+         I was changed on the Feature Branch
         </a>
       </header>
     </div>
